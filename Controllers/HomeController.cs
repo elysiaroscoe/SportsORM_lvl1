b20e116c97fcbb8afbba9dd3bc6759e6c5346bde
Roscoe <elysiaroscoe@gmail.com>
2021-12-08 16:30:23 -0800

    level1 done

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -40,7 +40,55 @@ namespace SportsORM.Controllers
                 .ToList();
 
             ViewBag.NotFootball = _context.Leagues
-                .Where(l => l.Sport.Contains("Football"))
+                .Where(l => !l.Sport.Contains("Football"))
+                .ToList();
+
+            ViewBag.ConferenceLeagues = _context.Leagues
+                .Where(l => l.Name.Contains("Conference"))
+                .ToList();
+
+            ViewBag.AtlanticLeagues = _context.Leagues
+                .Where(l => l.Name.Contains("Atlantic"))
+                .ToList();
+
+            ViewBag.DallasTeams = _context.Teams
+                .Where(l => l.Location.Contains("Dallas"))
+                .ToList();
+
+            ViewBag.RaptorsTeams = _context.Teams
+                .Where(l => l.TeamName.Contains("Raptors"))
+                .ToList();
+
+            ViewBag.CityTeams = _context.Teams
+                .Where(l => l.Location.Contains("City"))
+                .ToList();
+
+            ViewBag.TTeams = _context.Teams
+                .Where(l => l.TeamName.StartsWith("T"))
+                .ToList();
+
+            ViewBag.AlphabeticalTeamsbyLocation = _context.Teams
+                .OrderBy(l => l.Location)
+                .ToList();
+
+            ViewBag.ReverseAlphabeticalTeamsByName = _context.Teams
+                .OrderByDescending(l => l.TeamName)
+                .ToList();
+
+            ViewBag.Coopers = _context.Players
+                .Where(l => l.LastName.Contains("Cooper"))
+                .ToList();
+
+            ViewBag.Joshuas = _context.Players
+                .Where(l => l.FirstName.Contains("Joshua"))
+                .ToList();
+            
+            ViewBag.CoopersNoJoshua = _context.Players
+                .Where(l => l.LastName.Contains("Cooper") && !l.FirstName.Contains("Joshua"))
+                .ToList();
+
+            ViewBag.AlexanderOrWyatt = _context.Players
+                .Where(l => l.FirstName.Contains("Alexander") || l.FirstName.Contains("Wyatt"))
                 .ToList();
 
 
